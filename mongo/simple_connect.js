a798ddfed0499315ec9390bc80fbf71e1c9bf7eb
shreeka1 <shreekanth_venkateshappa@mcafee.com>
2019-01-22 01:49:36 -0800

    modified require statement to include ES6 syntax

--- mongo/simple_connect.js
+++ mongo/simple_connect.js
@@ -1,16 +1,31 @@
-var mongo_client = require('mongodb').MongoClient;
+var {MongoClient} = require('mongodb');
 
-console.log(mongo_client);
+console.log(MongoClient);
 
 // connect to some database inside mongo server
 
-mongo_client.connect('mongodb://localhost:27017/shreedb', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/shreedb', (err, db) => {
         if(err) {
                 return console.log('could not connect to mongo server');
         }
 
         console.log(db);
         console.log('connected to mongo server');
+
+        var doc = {
+                name : 'shree',
+                age : 36
+        };
+
+        // insert a doc into db
+        db.collection('shreeCol').insertOne(doc, (err, result) => {
+                if(err) {
+                        return console.log('could not insert document');
+                }
+
+                console.log('document inserted successfully')
+                console.log(result.ops[0]._id.getTimestamp());
+        });
 
         // close db
         db.close();
