e3b3c9742367dac9cc003b78974117cad2860500
shreesep82 <shree.sep82@gmail.com>
2019-02-01 07:11:06 +0530

    added rest server test file for post

--- mongo/restapi/rest_server.test.js
+++ mongo/restapi/rest_server.test.js
@@ -4,18 +4,24 @@ const request = require('supertest');
 const {app} = require('./rest_server');
 const {docStruct} = require('./rest_server')
 
+var sleep = require('sleep')
+
 // make sure to empty mongo db collection in the begining
 beforeEach((done) => {
-  docStruct.deleteMany({}).then(() => {
-    console.log('remove');
-    done()
-  })
+	docStruct.deleteMany({}, (err) => {
+		if(err) {
+			return console.log('find and remove: ', err);
+		}
+
+		console.log('user deleted');
+		done();
+	});
+
 })
-
-
 
 describe('Test rest server end points by posting some data', () => {
   it('test /rp_api1', (done) => {
+
 
     //send request and check it's response
     request(app)
@@ -25,27 +31,22 @@ describe('Test rest server end points by posting some data', () => {
       .expect((res) => {
         expect(res.body.name).toBe('new_name2')
       })
-
-
       .end((err, res) => {
         if(err) return done(err);
-        done();
+
+			docStruct.find().then((docs) => {
+				console.log('docs in db: ', docs)
+				expect(docs.length).toBe(1)
+				expect(docs[0].name).toBe('new_name2')
+				done();
+			})
+			.catch((err) => {
+				done(err);
+			})
+
       })
 
-//console.log('find: ', docStruct.findOne({'name': 'new_name2'}));
 
-/*
-      // check if mongo db got one entry after inserting above document
-      docStruct.find().then((docs) => {
-        console.log('docs: ', docs)
-        expect(docs.length).toBe(1)
-        expect(docs[0].name).toBe('new_name2')
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      })
-*/
   })
 
 })
