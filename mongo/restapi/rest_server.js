582c132f0afd7ea5f83b341dfb5d1759025883eb
shreeka1 <shreekanth_venkateshappa@mcafee.com>
2019-02-06 09:27:40 -0800

    added delete and update mocha files

--- mongo/restapi/rest_server.js
+++ mongo/restapi/rest_server.js
@@ -71,5 +71,113 @@ app.get('/rp_get_ap1/:id', (req, res) => {
         }
 });
 
+app.get('/rp_delete_ap1/:id', (req, res) => {
+        console.log('get by id');
+
+        var id = req.params.id
+
+        // validate id
+        if(ObjectId.isValid(id)) {
+                // get object from mongo
+                docStruct.findByIdAndDelete(id).then( (doc) => {
+                        if(doc == null) {
+                                console.log('id not found');
+                                res.status(404).send({err: 'Id not found'});
+                                return;
+                        }
+
+                        res.status(200).send(doc);
+                })
+                .catch( (err) => {
+                        console.log('mongo find failed');
+                        res.status(400).send({err: 'mongo find failed'});
+                });
+        }
+        else {
+                console.log('invalid id');
+                res.status(400).send({err: 'Invalid id'});
+        }
+});
+
+/*
+               {
+                        _id : 123
+                },
+                {
+                        $set : {
+                                name : 'shreekanth'
+                        },
+                        $inc : {
+                                age : 2
+                        }
+                },
+                {
+                        returnOriginal : false
+                },
+        ).then( (result) => {
+                console.log(result);
+        })
+        .catch( (err) => {
+                console.log(err);
+        });
+
+*/
+
+const lodash = require('lodash');
+
+app.patch('/rp_update_ap1/:id', (req, res) => {
+
+        console.log('get by id');
+
+        //var body = lodash.pick(req.body, ['name', 'completed']);
+        //body.name = "kan"
+        //body.timestamp = new Date();
+        //console.log('body: ', body);
+        //res.send();
+        //return;
+
+        var id = req.params.id
+
+        // validate id
+        if(ObjectId.isValid(id)) {
+                // get object from mongo
+                docStruct.findByIdAndUpdate(
+                        id,
+                        {
+                                $set : {
+                                        name : "kanth",
+                                        timestamp : new Date()
+                                },
+                                $inc : {
+                                        age : 0
+                                }
+                        },
+                        {
+                                new : true,
+                                strict : false
+                        }
+
+                )
+                .then( (doc) => {
+                        if(doc == null) {
+                                console.log('id not found');
+                                res.status(404).send({err: 'Id not found'});
+                                return;
+                        }
+
+                        //doc.set('f1', 'v1');
+                        res.status(200).send(doc);
+                })
+                .catch( (err) => {
+                        console.log('mongo find failed');
+                        res.status(400).send({err: 'mongo find failed'});
+                });
+        }
+        else {
+                console.log('invalid id');
+                res.status(400).send({err: 'Invalid id'});
+        }
+});
+
 module.exports.app = app;
 module.exports.docStruct = docStruct;
